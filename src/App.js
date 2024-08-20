929c662e424b2053ce52eaf43116a00e9c1f0b60
maydindev <maydin.dev@outlook.com>
2024-08-20 12:37:58 +0300

    Updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import { useRef } from 'react'
+import { useRef,useState } from 'react'
 
 export default function VideoPlayer() {
-  const ref = useRef()
+  const [isPlaying,setIsPlaying] = useState(false)
+  const ref = useRef(null)
 
   console.log(ref.current)
 
+  const handlePlay = () => {
+    ref.current.play()
+    setIsPlaying(true)
+  }
+
+  const handlePause = () => {
+    ref.current.pause()
+    setIsPlaying(false)
+  }
+
   return (
     <div className='max-w-[500px] space-y-4 p-8 mx-auto'>
-      <h1 ref={ref} className='text-center font-bold text-3xl'>
+      <h1 className='text-center font-bold text-3xl'>
         Video Oynatıcı
       </h1>
-      <video
+      <video ref={ref}
         src='https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4'
         poster='https://peach.blender.org/wp-content/uploads/title_anouncement.jpg?x11217'
       />
       <div className='flex justify-between'>
-        <button className='text-orange-500 font-semibold'>Duraklat</button>
-        <div>Duraklatıldı</div>
-        <button className='text-indigo-500 font-semibold'>Oynat</button>
+        <button onClick={handlePause} className='text-orange-500 font-semibold'>Duraklat</button>
+        <div>{isPlaying ? "Oynatılıyor" : "Duraklatıldı"}</div>
+        <button onClick={handlePlay} className='text-indigo-500 font-semibold'>Oynat</button>
       </div>
     </div>
   )
